4983068a2497ea892855a14a8542e3f10d362ee0
veeradevops26 <veeradevops26@gmail.com>
2026-05-15 17:42:42 +0530

    Update hello.sh added more lines --veer

--- hello.sh
+++ hello.sh
@@ -1,3 +1,8 @@
 this is from git hub.
 we are cloning this.
 commting from linux meachine
+this is from git hub.
+we are cloning this.
+commting from linux methis is from git hub.
+we are cloning this.
+commting from linux meachineachine
